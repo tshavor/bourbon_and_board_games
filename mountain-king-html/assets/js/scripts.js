3f084f95f50dfb9b8e69625e5be04357126e0b8c
tshavor <thomasshavor@gmail.com>
2017-02-04 16:10:09 -0600

    odometer working and star wars ipsum added

--- mountain-king-html/assets/js/scripts.js
+++ mountain-king-html/assets/js/scripts.js
@@ -4,10 +4,10 @@
 //===================
 
 setTimeout(function(){
-  $('.odometer.01').html(23578);
+  $('.odometer.01').html(500);
 }, 1000);
 setTimeout(function(){
-  $('.odometer.02').html(12702);
+  $('.odometer.02').html(1000);
 }, 1000);
 
 
